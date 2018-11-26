2a978c78f22e5c1f2ce64f9bf921dcf9dca5cab7
Edward <estoutenburg@datasystemservicesllc.com>
2018-11-26 15:51:42 -0500

    header enhancements partiall complete

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import MobileMenu from "./modules/mobile-menu";
 import RevealOnScroll from "./modules/revealOnScroll";
 import $ from "jquery";
+import StickyHeader from "./modules/stickyHeader";
 
 const mobileMenu = new MobileMenu();
 const revealFeatureOnScroll = new RevealOnScroll($(".feature-item"), "85%");
 const revealTestimonialOnScroll = new RevealOnScroll($(".testimonial"), "60%");
+const stickyHeader = new StickyHeader();
