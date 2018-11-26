eb386d41d4fcb72c42d1184aeb8b4335a93a686e
Edward <estoutenburg@datasystemservicesllc.com>
2018-11-26 08:54:21 -0500

    Header complete for large screens

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,12 +4,13 @@ const gulp = require("gulp"),
   cssvars = require("postcss-simple-vars"),
   nested = require("postcss-nested"),
   cssimport = require("postcss-import"),
-  mixins = require("postcss-mixins");
+  mixins = require("postcss-mixins"),
+  hexrgba = require("postcss-hexrgba");
 
 gulp.task("styles", () => {
   return gulp
     .src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssimport, mixins, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssimport, mixins, cssvars, nested, hexrgba, autoprefixer]))
     .on("error", function(error) {
       console.log(error.toString());
       this.emit("end");
