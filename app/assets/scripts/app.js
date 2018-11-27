cb6610863bfdd049b7714712f46441c4ccd4697c
Edward <estoutenburg@datasystemservicesllc.com>
2018-11-27 11:09:40 -0500

    modal complete

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,8 +2,10 @@ import MobileMenu from "./modules/mobile-menu";
 import RevealOnScroll from "./modules/revealOnScroll";
 import $ from "jquery";
 import StickyHeader from "./modules/stickyHeader";
+import Modal from "./modules/modal";
 
 const mobileMenu = new MobileMenu();
 const revealFeatureOnScroll = new RevealOnScroll($(".feature-item"), "85%");
 const revealTestimonialOnScroll = new RevealOnScroll($(".testimonial"), "60%");
 const stickyHeader = new StickyHeader();
+const modal = new Modal();
