03d77344a2a3a0481cbbc18604fbf96964f52178
Edward <estoutenburg@datasystemservicesllc.com>
2018-11-27 08:49:30 -0500

    sticky header completed

--- app/assets/scripts/modules/stickyHeader.js
+++ app/assets/scripts/modules/stickyHeader.js
@@ -1,12 +1,21 @@
 import $ from "jquery";
 import waypoints from "../../../../node_modules/waypoints/lib/noframework.waypoints";
+import smoothScroll from "jquery-smooth-scroll";
 
 class StickyHeader {
   constructor() {
     this.siteHeader = $(".site-header");
     this.headerTrigger = $(".large-hero__title");
     this.createWaypoint();
+    this.pageSections = $(".page-section");
+    this.createPageSectionWaypoints();
+    this.headerLinks = $(".primary-nav a");
+    this.addSmoothScrolling();
   }
+  addSmoothScrolling() {
+    this.headerLinks.smoothScroll();
+  }
+
   createWaypoint() {
     let that = this;
     new Waypoint({
@@ -18,6 +27,39 @@ class StickyHeader {
           that.siteHeader.removeClass("site-header--dark");
         }
       }
+    });
+  }
+
+  createPageSectionWaypoints() {
+    let that = this;
+    this.pageSections.each(function() {
+      let currentSection = this;
+      new Waypoint({
+        element: currentSection,
+        handler: function(direction) {
+          if (direction == "down") {
+            let matchingHeaderLink = currentSection.getAttribute(
+              "data-matching-link"
+            );
+            that.headerLinks.removeClass("current-link");
+            $(matchingHeaderLink).addClass("current-link");
+          }
+        },
+        offset: "18%"
+      });
+      new Waypoint({
+        element: currentSection,
+        handler: function(direction) {
+          if (direction == "up") {
+            let matchingHeaderLink = currentSection.getAttribute(
+              "data-matching-link"
+            );
+            that.headerLinks.removeClass("current-link");
+            $(matchingHeaderLink).addClass("current-link");
+          }
+        },
+        offset: "-40%"
+      });
     });
   }
 }
